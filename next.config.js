24c8d9552a094c145e4bf1892f8f78d0ceda7a41
davidyfxu <82054510@qq.com>
2022-07-03 20:08:39 +0800

    feat: clear2

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-const semi = require("@douyinfe/semi-next").default({
-  /* the extension options */
-});
-module.exports = semi(nextConfig);
